b5d10326e3dff530dfd78d4089339fe5356ce404
Jake Tae <jaesungtae@gmail.com>
2021-03-28 12:49:26 +0900

    debug: rm default debug port

--- utils.py
+++ utils.py
@@ -23,7 +23,7 @@ def create_browser(driver_path):
     options.add_argument("--no-sandbox")
     options.add_argument("disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--remote-debugging-port=9222")
+    # options.add_argument("--remote-debugging-port=9222")
     options.add_argument("window-size=1920,1080")
     browser = webdriver.Chrome(executable_path=driver_path, options=options)
     return browser
